b915b3cda9aa9af173cd0f4268e198eb9e0ffffd
eyrambernard300 <beyram300@gmail.com>
2023-09-19 13:12:49 +0000

    update:on serve static and view engine

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv')
-const {adminRoute,frontendRoute} = require('./routes')
+const {adminRoute,frontendRoute} = require('./routes');
+const expressEjsLayouts = require('express-ejs-layouts');
 
 // configuration .env
 dotenv.config();
@@ -10,6 +11,14 @@ const port = process.env.SERVER_PORT || 2020
 //server
 const app = express();
 
+// serve static
+app.use(express.static(__dirname+'/public'));
+
+
+// setting view engine
+app.set('view', 'ejs');
+
+app.use(expressEjsLayouts())
 //frontendRoute
 app.use('/', frontendRoute)
 //adminRoute
@@ -21,8 +30,8 @@ const startServer = ()=>{
         app.listen(port,()=>{
             console.log(`server runing on http://localhost:${port}`)
         })
-    } catch (e) {
-        console.log(e)
+    } catch (error) {
+        console.log(error)
         
     }
 }
